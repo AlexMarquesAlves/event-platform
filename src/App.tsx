83a509acfb8c5b53edaae30fd528689599dc9f00
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-06-21 02:32:23 -0300

    Feat: first look on queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { gql } from "@apollo/client";
+import { useEffect } from "react";
+import { client } from "./lib/apollo";
+
+const GET_LESSONS_QUERY = gql`
+   query {
+      lessons {
+         id
+         tittle
+
+         # teacher {
+         #    name
+         #    bio
+         # }
+      }
+   }
+`;
+
 function App() {
-  return (
-    <h1 className="text-5xl font-bold text-violet-500">Hello Ignite Lab</h1>
-  );
+   useEffect(() => {
+      client
+         .query({
+            query: GET_LESSONS_QUERY,
+         })
+         .then((res) => {
+            console.log(res.data);
+         });
+
+      return () => {};
+   }, []);
+
+   return (
+      <h1 className="text-5xl font-bold text-violet-500">Hello Ignite Lab</h1>
+   );
 }
 
 export default App;
