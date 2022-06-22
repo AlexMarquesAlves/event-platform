1055beb6c8ef0c55956cef497fa11a54d1c1f83b
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-06-21 23:55:04 -0300

    Feat: simplify imports

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from "./Header";
+export { default as Lesson } from "./Lesson";
+export { default as Sidebar } from "./Sidebar";
+export { default as Video } from "./Video";
