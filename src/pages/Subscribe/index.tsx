dc7819355a8b96c74874d9db7ad88ddb034c4912
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-16 23:17:29 -0300

    Added right side form content

--- src/pages/Subscribe/index.tsx
+++ src/pages/Subscribe/index.tsx
@@ -3,7 +3,8 @@ import { Logo } from "../../components";
 export function Subscribe() {
    return (
       <div className="flex flex-col items-center min-h-screen bg-no-repeat bg-cover bg-blur ">
-         <div className="max-w-[1100px] flex items-center  justify-between mt-20 mx-auto">
+         <div className="w-full max-w-[1100px] flex items-center  justify-between mt-20 mx-auto">
+            {/* Left side */}
             <div className=" max-w-[640px]">
                <Logo />
                <h1 className="mt-8 text-[2.2rem] md:text-[2.5rem] leading-tight text-black dark:text-gray-100">
@@ -22,7 +23,31 @@ export function Subscribe() {
                   melhores oportunidades do mercado.
                </p>
             </div>
-            <div></div>
+            {/* Right Side */}
+            <div className="p-8 bg-gray-200 border border-gray-700 rounded dark:bg-gray-700 dark:border-gray-500">
+               <strong className="block mb-6 text-xl md:text-2xl">
+                  Inscreva-se gratuitamente
+               </strong>
+               {/* Form */}
+               <form className="flex flex-col w-full gap-2">
+                  <input
+                     className="px-5 rounded dark:bg-gray-900 h-14 placeholder:text-gray-700 dark:placeholder:text-gray-400"
+                     type="text"
+                     placeholder="Seu nome completo"
+                  />
+                  <input
+                     className="px-5 rounded dark:bg-gray-900 h-14 placeholder:text-gray-700 dark:placeholder:text-gray-400"
+                     type="email"
+                     placeholder="Digite o seu e-mail"
+                  />
+                  <button
+                     className="py-4 mt-4 text-sm font-bold uppercase transition-colors bg-green-600 rounded dark:bg-green-500 hover:bg-green-800 dark:hover:bg-green-700 disabled:opacity-50"
+                     type="submit"
+                  >
+                     Garantir minha vaga
+                  </button>
+               </form>
+            </div>
          </div>
          <img
             src="/src/assets/codeMockup.png"
